2508e27a9e7f8773b61dc3eeb836f7a490e4b03e
waspy <waspy@naver.com>
2021-02-16 13:14:29 +0900

    commit

--- src/main/java/org/zerock/domain/MerchanVO.java
+++ src/main/java/org/zerock/domain/MerchanVO.java
@@ -1,13 +1,15 @@
 package org.zerock.domain;
 
+import java.util.Date;
+
 public class MerchanVO {
 
-    private int merchanOid;
-
-    private String merchanName;
-
+    private int merchanOid; //PK, auto increment
+    private int userOid;
+    private int sort;
+    private String name;
     private String content;
-
     private int price;
+    private Date postDate;
 
 }
--- src/main/java/org/zerock/domain/UserVO.java
+++ src/main/java/org/zerock/domain/UserVO.java
@@ -2,6 +2,8 @@ package org.zerock.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class UserVO {
     private int userOid; //PK, auto increment
@@ -9,6 +11,9 @@ public class UserVO {
     private String userPw;
     private String email;
     private String phone;
+    private String birth;
     private int gender; // 1=MAN 2=WOMAN
     private int auth; // 0=ADMIN 1=USER 2=SELLER
+
+    private Date regDate;
 }
